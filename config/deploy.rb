c9f8b2a04e247235a55a2c7adea9af66c0ff8d7f
Michele Franzin <michele.franzin@gmail.com>
2011-10-07 12:04:59 +0200

    capified!

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "r4r"
+set :user, "deploy"
+
+# repo details
+set :scm, :git
+set :branch, 'master'
+set :repository, "#{user}@fuzziland.it:/srv/repos/git/#{application}.git"
+
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :deploy_to, "/srv/rails/#{application}"
+set :deploy_via, :remote_cache  # if your server has direct access to the repository
+
+require 'bundler/capistrano'
+
+# RVM bootstrap
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.2'
+
+server "fuzziland.it", :app, :web, :db, :primary => true
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
