2ea332915e8c161f284bed8df38a06f25a172c7e
Michele Franzin <michele.franzin@gmail.com>
2011-10-08 12:25:20 +0200

    validates unique twitter + email

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
   validates :name, :presence => true, :length => {:maximum => 200}
-  validates :twitter, :length => {:maximum => 100}
+  validates :twitter, :uniqueness => true, :length => {:maximum => 100}
   validates :level, :presence => true, :length => {:maximum => 50}
-  validates :email, :presence => true, :length => {:maximum => 250}, :email_format => true
+  validates :email, :uniqueness => true, :presence => true, :length => {:maximum => 250}, :email_format => true
 end
