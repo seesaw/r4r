c6278166579d94e54117efec582a8fb505805bf6
Michele Franzin <michele.franzin@gmail.com>
2011-10-08 17:12:11 +0200

    close button on flash messages

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module R4r
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] += %w(rails.validations rails.validations.custom)
+    config.action_view.javascript_expansions[:defaults] += %w(rails.validations rails.validations.custom bootstrap-alerts)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
