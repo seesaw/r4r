9232ccb6a73ccd5a3ce981be52f9cc2d39554320
pigats <andrea@fruktarbo.com>
2011-11-07 17:56:08 +0100

    work on r4r
    it's better to run a bundle install - I really can't work w/o haml &
    compass

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 R4r::Application.routes.draw do
   resources :students, only: [:new, :create]
   match '/faq' => "pages#faq"
+  match '/idea' => "pages#idea"
   match '/courses' => "pages#courses"
   root :to => "pages#index"
 end
